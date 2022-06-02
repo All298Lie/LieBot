b05b7d9b359788e592a52e71229b892f06e7622a
BuildTools <unconfigured@null.spigotmc.org>
2022-06-03 02:38:03 +0900

    봇의 상태메세지가 표시되도록 설정

--- src/main/java/me/all298lie/LieBot.java
+++ src/main/java/me/all298lie/LieBot.java
@@ -3,6 +3,7 @@ package me.all298lie;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
@@ -12,6 +13,7 @@ public class LieBot extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder.createLight(args[0], Collections.emptyList())
                 .setStatus(OnlineStatus.ONLINE)
+                .setActivity(Activity.playing("소아 염탐"))
                 .build();
     }
 }
