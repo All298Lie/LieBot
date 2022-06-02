803510d417e76f02feab32b7798799c544c371e9
BuildTools <unconfigured@null.spigotmc.org>
2022-06-03 02:37:38 +0900

    봇이 온라인 표시가 되도록 설정

--- src/main/java/me/all298lie/LieBot.java
+++ src/main/java/me/all298lie/LieBot.java
@@ -1,4 +1,17 @@
 package me.all298lie;
 
-public class LieBot {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import java.util.Collections;
+
+public class LieBot extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createLight(args[0], Collections.emptyList())
+                .setStatus(OnlineStatus.ONLINE)
+                .build();
+    }
 }
