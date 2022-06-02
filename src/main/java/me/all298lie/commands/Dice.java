8bbbca5493d4b40692d14ab3cf717b29d484582a
BuildTools <unconfigured@null.spigotmc.org>
2022-06-03 04:04:24 +0900

    /주사위 명령어 추가

---
+++ src/main/java/me/all298lie/commands/Dice.java
@@ -0,0 +1,21 @@
+package me.all298lie.commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class Dice {
+    public static void run(SlashCommandInteractionEvent event) {
+        int value = (int) (Math.random() * 100) + 1;
+        MessageEmbed embed = new EmbedBuilder()
+                .setTitle(":game_die: 랜덤 주사위")
+                .setDescription("주사위 눈금 : " + value)
+                .setFooter(event.getUser().getAsTag())
+                .setColor(0x00b4d8)
+                .build();
+        Message message = new MessageBuilder(embed).build();
+        event.reply(message).queue();
+    }
+}
